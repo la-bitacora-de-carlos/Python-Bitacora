f7b1f9083a90d51b01d521fad123380188fe0aab
la-bitacora-de-carlos <labitacoradecarlosmorales@gmail.com>
2026-08-17 04:11:18 +0000

    Tipos de datos Simples

---
+++ SoyDato/TiposDeDatos/tipos-datos-simples.py
@@ -0,0 +1,8 @@
+'''
+Tipos de datos simples o primitivos en Python
+
+1- String (Cadenas de texto): "Hola", 'Python'
+2- Integer (Enteros): 10, -5
+3- Float (Números flotantes o decimales): 3.14, -0.5
+4- Boolean (Estados binarios (True o False) 1 o 0): True, False
+'''
